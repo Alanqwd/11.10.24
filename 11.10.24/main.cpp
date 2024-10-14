7ed32c38fdded97722f08a8a2d11228315050bf8
Нурбек <162671963+Alanqwd@users.noreply.github.com>
2024-10-14 19:51:21 +0500

    Update main.cpp

--- 11.10.24/main.cpp
+++ 11.10.24/main.cpp
@@ -1,176 +1,96 @@
 #include <iostream>
-#include <Windows.h>
-#include <string>
 #include <vector>
+#include <string>
 
-
-
-// онкхлнптхгл - реумнкнцхъ хглемвхбнцн онбедемхъ опнцпюллш опх кхмеимни янупюмемхе ярпсйрспш йндю 
-// опх онкхлнптмшу бшгнбюу опнцпюллю пюявхршюъ мю йнд нохяюммши б пндхрекэяйнл йкюяяе тюйрхвеяйх асдер бшонкмърэ йнд лернднб нохяюммшу б йкюяяюу мюякедмхйюу
-//
-//
-//
-  
-// дкъ рнцн врнаш онкхлнптхгл пюанрюк днкфмю ясыеярбнбюрэ бхпрсюкэмюъ рюакхжю рхонб
-class Base
-{
-public:
-	Base() {
-		std::cout << "Base construct\n";
-	}
-	virtual ~Base() {
-		std::cout << "Base destroyed\n";
-	}
-		// йкчвебне якнбн virtual янгдюер бхпрсюкэмсч рюакхжс рхонб б опнцпюлле 
-	virtual void PrintW() const { std::cout << "Hello world\n"; }
-
-
-private:
-
-};
-
-class Derived:public Base{ 
-public:    
-	Derived() {
-		std::cout << "Derived construct\n";
-	}
-	~Derived() {
-		std::cout << "Derived destroyed\n";
-	}
-	void PrintW() const override { std::cout << "Hello world 2\n"; }
-
-
-};
-// онкхлнптмше бнглнфмш рнкэйн йнцдю назейрш б опнцпюлле он яяшкйюл х сйюгюрекъл 
-
-
-void Foo(Base &a)
-{
-	a.PrintW();
-}
 class Plant {
-public:
-
-	Plant(const std::string& name, const std::string& size, float height)
-		: name(name), size(size), height(height) {}
-
-
-	virtual void info() const {
-		std::cout << "мЮГБЮМХЕ: " << name << "\n"
-			<< "пЮГЛЕП: " << size << "\n"
-			<< "бШЯНРЮ: " << height << " Б ЛЕРПЮУ\n";
-	}
 protected:
-	std::string name;
-	std::string size;
-	float height;
+    std::string name;
+    std::string size;  
+    double height;
 
+public:
+    Plant(std::string n, std::string s, double h)
+        : name(n), size(s), height(h) {}
 
+    virtual void collectFruits() = 0;
+
+    virtual ~Plant() = default; 
 };
 
-
-class The_shrub : public Plant {
+class Bush : public Plant {
 public:
+    Bush(std::string n, double h) : Plant(n, "п°п╟п╩п╣п╫я▄п╨п╦п╧", h) {}
 
-	The_shrub(const std::string& name, float height)
-		: Plant(name, "лЮКЕМЭЙХИ", height) {}
-
-
-	void info() const override {
-		Plant::info();
-	}
+    void collectFruits() override {
+        std::cout << "п║п╠п╬я─ я▐пЁп╬п╢ я│ п╨я┐я│я┌п╟: " << name << "\n";
+    }
 };
 
 class Tree : public Plant {
+private:
+    int maxFruits; 
 public:
+    Tree(std::string n, double h, int maxFruits)
+        : Plant(n, "п▒п╬п╩я▄я┬п╬п╧", h), maxFruits(maxFruits) {}
 
-	Tree(const std::string& name, float height)
-		: Plant(name, "аНКЭЬНИ", height) {}
+    void collectFruits() override {
+        std::cout << "п║п╠п╬я─ п©п╩п╬п╢п╬п╡ я│ п╢п╣я─п╣п╡п╟: " << name << "\n";
+        for (int i = 0; i < maxFruits; ++i) {
+            if (i % 2 == 0) {
+                std::cout << "п╓я─я┐п╨я┌ " << i + 1 << "\n";
+            }
+        }
+    }
 
-
-	void info() const override {
-		Plant::info();
-	}
+    std::vector<std::string> getFruits() {
+        std::vector<std::string> fruits;
+        for (int i = 0; i < maxFruits; ++i) {
+            fruits.push_back("п╓я─я┐п╨я┌ " + std::to_string(i + 1));
+        }
+        return fruits;
+    }
 };
 
 
-
-
-
-
-
-class fetus {
-public:
-
-	fetus(const std::string& name, float weight)
-		: name(name),
-		weight(weight){}
-
-
-	virtual void info() const {
-		std::cout << "мЮГБЮМХЕ: " << name << "\n"
-			<< "бЕЯ: " << weight << " ЙЦ\n";
-	}
+class Fruit {
 protected:
-	std::string name; 
-	float weight;
+    std::string name;
+    double weight;
 
-
-};
-
-
-class blueberries : public fetus {
 public:
-
-	blueberries(const std::string& name, float weight)
-		: fetus(name,weight) {}
-
-
-	void info() const override {
-		fetus::info();
-	}
+    Fruit(std::string n, double w) : name(n), weight(w) {}
+    virtual ~Fruit() = default;
 };
 
-class banana : public fetus{
+
+class Berry : public Fruit {
 public:
-
-	banana(const std::string& name, float weight)
-		: fetus(name,weight) {}
-
-
-	void info() const override {
-		fetus::info();
-	}
+    Berry(std::string n, double w) : Fruit(n, w) {}
 };
 
+class FruitType : public Fruit {
+public:
+    FruitType(std::string n, double w) : Fruit(n, w) {}
+};
 
-// еякх оепедювю бедеряъ  он оюпюлерпс рн опнхяундхр ноепюжхъ upcast йнрнпюъ нряейюер нр бяе ябниярбю мюякедмшу йкюяянб х янгдюер мнбши назейр аюгнбнцн 
-// мю ху нямнбе 
+class Cone : public Fruit {
+public:
+    Cone(std::string n, double w) : Fruit(n, w) {}
+};
+
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	/*Base * a= new Base;
-	a->PrintW();
-	Base * A = new Derived;
-	A->PrintW();
-	Foo(*a);
-	Foo(*A);
-	delete a;
-	delete A;*/
-	The_shrub shrub("йСЯР", 1.5);
-	Tree tree("дЕПЕБН", 7);
-	// бШБНД ХМТНПЛЮЖХХ Н ПЮЯРЕМХЪУ
-	std::cout << "хМТНПЛЮЖХЪ Н ЙСЯРЕ:\n";
-	shrub.info();
-	std::cout << "\nхМТНПЛЮЖХЪ Н ДЕПЕБЕ:\n";
-	tree.info();
-	blueberries bb("вЕПМХЙЮ", 1);
-	banana banana("аЮМЮМ", 5);
-	// бШБНД ХМТНПЛЮЖХХ Н ПЮЯРЕМХЪУ
-	std::cout << "хМТНПЛЮЖХЪ Н ВЕПМХЙЕ:\n";
-	bb.info();
-	std::cout << "\nхМТНПЛЮЖХЪ Н АЮМЮМЕ:\n";
-	banana.info();
+    setlocale(LC_ALL, "ru");
+    Bush myBush("п╞пЁп╬п╢п╟", 1.5);
+    myBush.collectFruits();
 
-	return 0;
+    Tree myTree("п■п╣я─п╣п╡п╬", 3.0, 5);
+    myTree.collectFruits();
+
+    // п÷п╬п╩я┐я┤п╣п╫п╦п╣ п©п╩п╬п╢п╬п╡ п╢п╣я─п╣п╡п╟
+    auto fruits = myTree.getFruits();
+    for (const auto& fruit : fruits) {
+        std::cout << fruit << " п║п╬п╠п╦я─п╟я▌я┌ я│ п╢п╣я─п╣п╡п╟" << "\n";
+    }
+
+    return 0;
 }
